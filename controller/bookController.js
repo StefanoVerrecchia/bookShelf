d50926054c9c2665c0330932aa4700ab59e3de7e
SVerr <stefanoverrecchia96@gmail.com>
2024-05-02 16:42:45 +0200

    Refactoring using express-toolkit

--- controller/bookController.js
+++ controller/bookController.js
@@ -1,81 +1,8 @@
 const Book = require('../model/bookModel.js');
 const { Controller } = require('express-toolkit')
 
-/* const controller = new Controller({
+const controller = new Controller({
     model : Book,
     name : 'books'
-}); */
-
-
-exports.getAllBook = async (req, res) => {
-    try {
-        const data = await Book.find();
-
-        console.log('DATA');
-        console.log(data);
-
-        res.json({ status: 'Success', data: data })
-    } catch (error) {
-        console.log(error);
-        res.json({ status: 'Fail', data: error })
-
-    }
-};
-exports.getSingleBook = async (req, res) => {
-    try {
-        console.log(req.params.id);
-        const data = await Book.findById(req.params.id);
-
-
-        res.json({ status: 'Success', data: data })
-    } catch (error) {
-        console.log(error);
-        res.json({ status: 'Fail', data: error })
-
-    }
-
-};
-exports.createBook = async (req, res) => {
-    try {
-        const data = await Book.create(req.body);
-        res.json({ status: 'Success', data: data })
-
-    } catch (error) {
-        console.log(error);
-        res.json({ status: 'Fail', data: error })
-
-    }
-
-};
-exports.updateBook = async (req, res) => {
-    try {
-        await Book.findByIdAndUpdate(req.params.id,req.body);
-        res.json({ status: 'Success'})
-    } catch (error) {
-        console.log(error);
-        res.json({ status: 'Fail', data: error })
-    }
-
-};
-exports.deleteBook = async (req, res) => {
-    try {
-        await Book.findByIdAndDelete(req.params.id,req.body);
-        res.json({ status: 'Success'})
-    } catch (error) {
-        console.log(error);
-        res.json({ status: 'Fail', data: error })
-
-    }
-
-};
-exports.deleteAllBook = async (req, res) => {
-    try {
-        await Book.deleteMany({});
-        res.json({ status: 'Success'})
-    } catch (error) {
-        console.log(error);
-        res.json({ status: 'Fail', data: error })
-
-    }
-
-};
+});
+module.exports = controller;
--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,26 +1,6 @@
-const express = require('express');
-const router = express.Router();
-
-const controller = require('../controller/bookController.js');
-
-//GET
-router.route('/').
-    get(controller.getAllBook);
-router.route('/:id').
-    get(controller.getSingleBook);
-
-//POST
-router.route('/').
-    post(controller.createBook);
-
-//PUT
-router.route('/:id').
-    put(controller.updateBook);
-
-//DELETE
-router.route('/:id').
-    delete(controller.deleteBook);
-router.route('/').
-    delete(controller.deleteAllBook);
-
-module.exports = router;
+const { buildRouter } = require('express-toolkit')
+const booksController = require('../controller/bookController.js');
+module.exports = buildRouter({
+    controller: booksController,
+    options: {} // See expressJS router options
+})
